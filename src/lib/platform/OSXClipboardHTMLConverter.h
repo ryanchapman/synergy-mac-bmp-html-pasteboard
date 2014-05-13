158b24afbf6e928554db755ad9a9c085ed3770ce
Ryan <ryan@Ryans-MacBook-Pro.local>
2014-05-12 22:05:52 -0600

    Mac: add support for copy/pasting of images and HTML

---
+++ src/lib/platform/OSXClipboardHTMLConverter.h
@@ -0,0 +1,54 @@
+/*
+ * synergy -- mouse and keyboard sharing utility
+ * Copyright (C) 2012 Bolton Software Ltd.
+ * Copyright (C) 2002 Chris Schoeneman
+ *
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file COPYING that should have accompanied this file.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "OSXClipboardAnyTextConverter.h"
+
+//! Convert to/from HTML encoding
+class COSXClipboardHTMLConverter :
+                public COSXClipboardAnyTextConverter {
+public:
+    COSXClipboardHTMLConverter();
+    virtual ~COSXClipboardHTMLConverter();
+
+    // IMSWindowsClipboardConverter overrides
+    virtual IClipboard::EFormat
+                        getFormat() const;
+    //virtual UINT      getWin32Format() const;
+    
+    virtual CFStringRef
+                        getOSXFormat() const;
+
+protected:
+    // COSXClipboardAnyTextConverter overrides
+    virtual CString     doFromIClipboard(const CString&) const;
+    virtual CString     doToIClipboard(const CString&) const;
+
+    // generic encoding converter
+    static CString      convertString(const CString& data,  
+                            CFStringEncoding fromEncoding,
+                            CFStringEncoding toEncoding);
+
+/*private:
+    CString             findArg(const CString& data, const CString& name) const;
+
+private:
+    UINT                m_format;
+*/
+};
